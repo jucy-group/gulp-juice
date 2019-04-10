34b0c2431ede08785dc858b4cc321cd58c578d3f
Corey Sewell <corey.sewell@jucyworld.com>
2019-04-10 12:34:41 +1200

    Remove gulp-util

--- test.js
+++ test.js
@@ -1,25 +1,23 @@
 var juice = require('./'),
     assert = require('assert'),
-    fs = require('fs'),
-    gutil = require('gulp-util');
+    fs = require('fs');
 
-describe('gulp-juice', function() {
-  it('should work in buffer mode', function(done) {
-    var buf = fs.readFileSync('./test.html'),
-        juice = require('./')({}).on('data', function(file){
-        var html = file.contents.toString();
-        assert(html.indexOf('<style') === -1,
-            'after juicing should have no style tag');
-    }).on('end', done);
+describe('gulp-juice', function () {
+    it('should work in buffer mode', function (done) {
+        var buf = fs.readFileSync('./test.html'),
+            juice = require('./')({}).on('data', function (file) {
+                var html = file.contents.toString();
+                assert(html.indexOf('<style') === -1,
+                    'after juicing should have no style tag');
+            }).on('end', done);
 
-    assert(buf.toString().indexOf('<style') > -1,
-        'should originally have style tag');
+        assert(buf.toString().indexOf('<style') > -1,
+            'should originally have style tag');
+        var wstream = fs.createWriteStream('test.html');
+        wstream.write(buf);
+        wstream.end();
 
-    juice.write(new gutil.File({
-        contents: buf,
-        path: 'test.html'
-    }));
 
-    juice.end();
-  });
+        juice.end();
+    });
 });
